567686c1eafb6149c83097569fe7fcb7d7c71976
KevinAcv2 <kevin_aacevedo@soy.sena.edu.co>
2025-05-08 17:40:07 +0200

    Camcion08

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+
 
 app = Flask(__name__)
 
-@app.route('/promedio-goles', methods=['GET', 'POST'])
+@app.route('/')
+def inicio():
+   return render_template('index.html')
+
+@app.route('/liga')
+def liga():
+   return render_template('LigaBetplay.html')
+
+@app.route('/copaB')
+def copaB():
+   return render_template('CopaBetplay.html')
+
+@app.route('/ligaE')
+def ligaE():
+   return render_template('LigaEspaña.html')
+
+@app.route('/premierL')
+def premierL():
+   return render_template('PremierLeage.html')
+
+@app.route('/championsL')
+def championsL():
+   return render_template('ChampionsLeage.html')
+
+@app.route('/promedio_goles', methods=['GET', 'POST'])
 def promedio_goles():
     promedio = None
     if request.method == 'POST':
@@ -16,7 +41,7 @@ def promedio_goles():
         except ValueError:
             promedio = "Error: Ingresa valores numéricos válidos"
 
-    return render_template('promedio_goles.html', promedio=promedio)
+    return render_template('LigaBetplay.html', promedio=promedio)
 
 if __name__ == '__main__':
     app.run(debug=True)
